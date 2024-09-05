3b6148f6d1d3ab0b02afbccfb4074a47ff58925e
Peter Knipp <peteraknipp@gmail.com>
2024-09-04 21:59:28 -0400

    Insert puts statements.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,15 +8,21 @@ class WelcomeController < ApplicationController
   # GET /:shape
   def show
     shape = params[:shape]
+    puts shape
     first_char = shape[0]
     shape = shape[1..-1]
+    puts shape
     second_char = shape[0]
     shape = shape[1..-1]
+    puts shape
     last_char = shape[-1]
     shape = shape[0..-2]
+    puts shape
     second_to_last_char = shape[-1]
     shape = shape[0..-2]
+    puts shape
     shape_arr = shape.split("),(")
+    puts shape_arr
     @triangle = shape[0]
     @tetrahedron = shape[1]
     render 'show'
