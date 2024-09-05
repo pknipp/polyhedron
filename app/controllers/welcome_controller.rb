643280f0d78b2eae2f575d0d2a1c1464255d670a
Peter Knipp <peteraknipp@gmail.com>
2024-09-04 21:56:32 -0400

    Use slices instead of chop and shift.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,10 +8,14 @@ class WelcomeController < ApplicationController
   # GET /:shape
   def show
     shape = params[:shape]
-    first_char = shape.shift
-    second_char = shape.shift
-    last_char = shape.chop
-    second_to_last_char = shape.chop
+    first_char = shape[0]
+    shape = shape[1..-1]
+    second_char = shape[0]
+    shape = shape[1..-1]
+    last_char = shape[-1]
+    shape = shape[0..-2]
+    second_to_last_char = shape[-1]
+    shape = shape[0..-2]
     shape_arr = shape.split("),(")
     @triangle = shape[0]
     @tetrahedron = shape[1]
