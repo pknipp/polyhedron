23348ad4a51685d31da73b96460e2ce7746d1ccb
Peter Knipp <peteraknipp@gmail.com>
2024-09-04 21:10:18 -0400

    Start parsing shape.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,13 @@ class WelcomeController < ApplicationController
   # GET /:shape
   def show
     shape = params[:shape]
-    puts shape
-    @shape = shape
+    first_char = shape.shift
+    second_char = shape.shift
+    last_char = shape.chop
+    second_to_last_char = shape.chop
+    shape_arr = shape.split("),(")
+    @triangle = shape[0]
+    @tetrahedron = shape[1]
     render 'show'
   end
 
