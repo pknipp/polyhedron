4ed0691a82ab25ef1239e52224e46119cb6c4dfc
Peter Knipp <peteraknipp@gmail.com>
2024-09-05 20:01:02 -0400

    Start working on TODO list.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,23 +8,17 @@ class WelcomeController < ApplicationController
   # GET /:shape
   def show
     shape = params[:shape]
-    puts shape
     first_char = shape[0]
     shape = shape[1..-1]
-    puts shape
     second_char = shape[0]
     shape = shape[1..-1]
-    puts shape
     last_char = shape[-1]
     shape = shape[0..-2]
-    puts shape
     second_to_last_char = shape[-1]
     shape = shape[0..-2]
-    puts shape
     shape_arr = shape.split("),(")
-    puts shape_arr
     @triangle = shape_arr[0]
-    @tetrahedron = shape_arr[1]
+    # @tetrahedron = shape_arr[1]
     render 'show'
   end
 
