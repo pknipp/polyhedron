10ad278436fec4e5de024104d78e222faf02023a
Peter Knipp <peteraknipp@gmail.com>
2024-09-06 09:25:52 -0400

    Input two vertices, properly positionned and scaled.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,6 @@ class WelcomeController < ApplicationController
 
   # GET /:shape
   def show
-    @size = 900
     shape = params[:shape]
     first_char = shape[0]
     shape = shape[1..-1]
@@ -19,9 +18,33 @@ class WelcomeController < ApplicationController
     shape = shape[0..-2]
     shape_arr = shape.split("),(")
     triangle = shape_arr[0].split(",")
+    svg_size = 900
+    @size = svg_size
     @vertex_names = triangle.first(3)
-    @edge_lengths = triangle.last(3)
-    # @tetrahedron = shape_arr[1]
+    edge_lengths = triangle.last(3)
+    vertices = [[0,0,0]]
+    vertices.push([edge_length[0], 0, 0])
+    mins = Array.new(3, Float::INFINITY)
+    maxs = Array.new(3, -Float::INFINITY)
+    vertices.each{|vertex|
+      (0..3).each{|i|
+        mins[i] = (mins[i], vertex[i]).min
+        maxs[i] = (maxs[i], vertex[i]).max
+      }
+    }
+    origin = []
+    size = 0
+    (0..3).each{|i|
+      origin.push((maxs[i] + mins[i]) / 2)
+      size = (size, maxs[i] - mins[i]).max
+    }
+    ratio = 0.8
+    vertices.each{|vertex|
+      (0..3).each{|i|
+        vertex[i] = ratio * (vertex[i] - origin[i]) * svg_size / size
+      }
+    }
+    @vertices = vertices
     render 'show'
   end
 
