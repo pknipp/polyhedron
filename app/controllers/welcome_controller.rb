1bbe79fc3a09fc1a7632a62e6e90e129fb0b2328
Peter Knipp <peteraknipp@gmail.com>
2024-09-12 19:03:48 -0400

    Add first real error message.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -42,8 +42,11 @@ class WelcomeController < ApplicationController
     # parse the entire url
     shape = params[:shape].gsub(/\s+/, "")
     first_char = shape[0]
-    @error = "Hello error"
-    render :error
+    if first_char != "["
+      @error = "The path's first character should be [, not " + first_char + "."
+      render :error
+      return
+    end
     return
     shape = shape[1..-1]
     second_char = shape[0]
