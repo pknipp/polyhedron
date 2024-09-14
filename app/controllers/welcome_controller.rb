4a5eb3e6dba2f282dbce4d491081775d9f516ea2
Peter Knipp <peteraknipp@gmail.com>
2024-09-14 10:39:44 -0400

    Use "return render :error" in order to consolidate two lines.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -51,22 +51,19 @@ class WelcomeController < ApplicationController
     second_char = shape[0]
     if second_char != "("
       @error = "The path's second character should be '(' not '" + second_char + "'."
-      render :error
-      return
+      return render :error
     end
     shape = shape[1..-1]
     last_char = shape[-1]
     if last_char != "]"
       @error = "The path's last character should be ']' not '" + last_char + "'."
-      render :error
-      return
+      return render :error
     end
     shape = shape[0..-2]
     second_to_last_char = shape[-1]
     if second_to_last_char != ")"
       @error = "The path's second to last character should be ')' not '" + second_to_last_char + "'."
-      render :error
-      return
+      return render :error
     end
     shape = shape[0..-2]
     shape_arr = shape.split("),(")
@@ -75,8 +72,7 @@ class WelcomeController < ApplicationController
     triangle = shape_arr[0].split(",")
     if triangle.length != 6
       @error = "The first element of the path's array should have 6 elements, not " + triangle.length.to_s + "."
-      render :error
-      return
+      return render :error
     end
     triangle_names = triangle.first(3)
     edge_lengths = triangle.last(3)
@@ -86,13 +82,11 @@ class WelcomeController < ApplicationController
     ab = Float(edge_lengths[0].sub('*', '.')) rescue nil
     if ab.nil?
       @error = "The path fragment " + edge_lengths[0] + " cannot be parsed as a number."
-      render :error
-      return
+      return render :error
     end
     if vertices.has_key?(first_name)
       @error = "The label " + first_name + " is used to label more than one vertex in this polyhedron."
-      render :error
-      return
+      return render :error
     else
       vertices[first_name] = Vertex.new([ab, 0, 0])
     end
@@ -100,20 +94,17 @@ class WelcomeController < ApplicationController
     first_name = triangle_names[2]
     if vertices.has_key?(first_name)
       @error = "The label " + first_name + " is used to label more than one vertex in this polyhedron."
-      render :error
-      return
+      return render :error
     end
     bc = Float(edge_lengths[1].sub('*', '.')) rescue nil
     if bc.nil?
       @error = "The path fragment " + edge_lengths[1] + " cannot be parsed as a number."
-      render :error
-      return
+      return render :error
     end
     ac = Float(edge_lengths[2].sub('*', '.')) rescue nil
     if ac.nil?
       @error = "The path fragment " + edge_lengths[2] + " cannot be parsed as a number."
-      render :error
-      return
+      return render :error
     end
     cx = (ac * ac + ab * ab - bc * bc) / 2 / ab
     cy = Math.sqrt(ac * ac - cx * cx)
@@ -125,8 +116,7 @@ class WelcomeController < ApplicationController
     tetrahedron = shape_arr[1].split(",")
     if tetrahedron.length != 7
       @error = "The second element of the path's array should have 7 elements, not " + tetrahedron.length.to_s + "."
-      render :error
-      return
+      return render :error
     end
     tetrahedron_names = tetrahedron.first(4)
     base_names = tetrahedron_names.first(3)
@@ -134,34 +124,29 @@ class WelcomeController < ApplicationController
     base_names.each do |base_name|
       if !vertices.has_key?(base_name)
         @error = "A vertex (" + base_name + ") named in the base (" + base + ") of a tetrahedron does not seem to match one of the existing ones ([" + vertices.keys.join(", ") + "])."
-        render :error
-        return
+        return render :error
       end
     end
     new_name = tetrahedron_names.last(1)[0]
     if vertices.has_key?(new_name)
       @error = "The label " + new_name + " is used to label more than one vertex in this polyhedron."
-      render :error
-      return
+      return render :error
     end
     edge_lengths = tetrahedron.last(3)
     ad = Float(edge_lengths[0].sub('*', '.')) rescue nil
     if ad.nil?
       @error = "The path fragment " + edge_lengths[0] + " cannot be parsed as a number."
-      render :error
-      return
+      return render :error
     end
     bd = Float(edge_lengths[1].sub('*', '.')) rescue nil
     if bd.nil?
       @error = "The path fragment " + edge_lengths[1] + " cannot be parsed as a number."
-      render :error
-      return
+      return render :error
     end
     cd = Float(edge_lengths[2].sub('*', '.')) rescue nil
     if cd.nil?
       @error = "The path fragment " + edge_lengths[2] + " cannot be parsed as a number."
-      render :error
-      return
+      return render :error
     end
     dx = (ab * ab + ad * ad - bd * bd) / 2 / ab
     s =  (ac * ac + ad * ad - cd * cd) / 2 / ac
