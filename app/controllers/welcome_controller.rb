aa0547b56eee719e2b5088c913ccff015f87b06a
Peter Knipp <peteraknipp@gmail.com>
2024-10-31 15:44:43 -0400

    Add p statements for vertex CZZ

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -415,10 +415,12 @@ class WelcomeController < ApplicationController
               coords[j][2],
             ]
           end
+          p tetrahedron.vertices[1].coords
           p tetrahedron.vertices[3].coords
 
           # back-translation
           (0..2).each {|k| tetrahedron.vertices.each {|vertex| vertex.coords[k] += origin[k]}}
+          p tetrahedron.vertices[1].coords
           p tetrahedron.vertices[3].coords
 
           # Insert one entry to vertices hashmap and three to edges hashmap.
